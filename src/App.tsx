1b79b0845d180bc884885186076fe00970a4a604
DanPark13 <dpark0703@gmail.com>
2023-09-25 11:39:36 -0400

    Base Project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Data Structures & Algorithms Flashcards</h1>
+     <h3>A Light Refresher on the Data Structures & Algorithms Needed for Technical Interviews</h3>
+    </>
+  )
+}
+
+export default App;
