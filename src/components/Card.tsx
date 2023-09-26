056c2c90108fac3f79698b38d2cbc3078068ce64
DanPark13 <dpark0703@gmail.com>
2023-09-26 15:53:21 -0400

    Add Previous/Next

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,13 @@ interface CardProps {
     answer: string;
 }
 
-const Card: React.FC<CardProps> = ({question, answer}) => {
+const Card: React.FC = () => {
+    const cards = [
+        { question: "What is a Data Structure?", answer: "A specific way of organizing and storing data in a computer so it can be accessed and modified efficiently." },
+        { question: "What is a Bit?", answer: "A unit of information stored as a 0 (FALSE), 1 (TRUE), or NULL." },
+    ];
+
+    const [activeCardIndex, setActiveCardIndex] = useState(0);
     const [isRevealed, setIsRevealed] = useState(false);
 
     const handleCardClick = () => {
@@ -14,9 +20,25 @@ const Card: React.FC<CardProps> = ({question, answer}) => {
     }
 
     return (
-        <div className={`card ${isRevealed ? 'revealed' : ''}`} onClick={handleCardClick}>
-            <div className="cardContent question">{question}</div>
-            <div className="cardContent answer">{answer}</div>
+        <div>
+            <div className={`card ${isRevealed ? 'revealed' : ''}`} onClick={handleCardClick}>
+                <div className="cardContent question">{cards[activeCardIndex].question}</div>
+                <div className="cardContent answer">{cards[activeCardIndex].answer}</div>
+            </div>
+
+            <button onClick={() => {
+                setIsRevealed(false); // Reset flip when navigating
+                setActiveCardIndex(prevIndex => Math.max(prevIndex - 1, 0));
+            }}>
+                Previous
+            </button>
+
+            <button onClick={() => {
+                setIsRevealed(false); // Reset flip when navigating
+                setActiveCardIndex(prevIndex => Math.min(prevIndex + 1, cards.length - 1));
+            }}>
+                Next
+            </button>
         </div>
     );
 }
